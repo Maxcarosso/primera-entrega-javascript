5b90981d1dce23f9721ebf68b126a40dfb09e3ba
Maximiliano Carosso <carosso47@gmail.com>
2024-10-31 22:41:33 -0300

    cambios en carrito, objetos, metodos y array para pre-entrega2

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,45 @@
-const prductos = [
-    {nombre:"nike", precio: 35},
-    {nombre: "adidas", precio: 30},
-    {nombre:"vans", precio: 40}
+class Usuario {
+    contructor(nombre, edad, nacionalidad)
+{   this.nombre = nombre;
+    this.edad = edad;
+    this.nacionalidad = nacionalidad;
+
+    this.saludar = funciton()
+    console.log("hola, soy " + this.nombre)
+}
+} 
+const cliente1 = new Usuario("maxi", 40, "argentina");
+const cliente2 = new Usuario("ana", 30, "argentina");
+const cliente3 = new Usuario("patri", 60, "argentina");
+console.log(cliente1);
+cliente1.saludar()
+
+for (const detalles in cliente1){
+    console.log(detalles);
+}
+
+
+
+
+const IVA = 1.21;
+
+const productos = [
+    {nombre:"nike", precio: 35000},
+    {nombre: "adidas", precio: 30000},
+    {nombre:"vans", precio: 40000},
 ];
+/*console.log(productos);
+console.log(productos[1])*/
+/*for (let i = 0; i <3; i++){
+    console.log[i];
+}*/
+console.log(productos.length);
+for (let i = 0; i < productos.length; i++){
+    console.log(productos[i]);
+}
+productos.push("converse", precio, 50000);
+console.log(productos);
+
 let carrito = []
 
 let seleccion= prompt ("hola desea comprar un pruducto? si o no?")
@@ -17,30 +54,36 @@ if (seleccion == "si")
 } else if (seleccion =="no"){
     alert ("gracias por visitarnos")
 }
-while (selecion !="no") {
+while (seleccion !="no") {
     let producto = prompt ("agrega un producto a tu carrito")
-    let precio = 0
-    if (producto == "nike" || productos == "adidas" || productos == "vans")
-    {
-        switch (productos) {case "nike": precio = 35
+    let precio = 0   
+    
+        switch (producto) {
+            case "nike": precio = 35000
             break;
-            case "adidas": precio = 30
+            case "adidas": precio = 30000
             break;
-            case "vans": precio = 40
+            case "vans": precio = 40000
             break;
+            case "converse": precio = 50000
             default:
-                break;
-                let unidades = parseInt (prompt("cuantas unidades quiere llevar"));
+            alert ("no tenemos ese preoducto")  
+        }
+        if (precio !=0) {
+        let unidades = parseInt (prompt("cuantas unidades quiere llevar"));
                 carrito.push({productos, unidades, precio})
                 console.log (carrito)
-        }
-    }
-        else {alert ("no tenemos ese preoducto")}
+            }
+        
         seleccion = prompt ("desea otro producto?")
-        while (seleccion == "no"){alert ("gracias por su compra")
-            carrito.forEach((carritofinal) => {console.log (`producto: $ { carritofinal.productos}, unidades: ${carritofinal.unidades}, total a pagar ${carritofinal.unidades*carritofinal.precio}`)} )
-        break;
     }
-}
-const total = carrito.reduce ((ecc,el) => acc + el.precio* el.unidades, 0)
+        if (carrito.length > 0){
+        alert ("gracias por su compra")
+            carrito.forEach((carritofinal) => {
+        console.log (`producto: ${carritofinal.productos}, unidades: ${carritofinal.unidades}, total a pagar ${carritofinal.unidades * carritofinal.precio}`)} )
+        }
+        
+    
+
+const total = carritofinal.reduce ((ecc,el) => acc + el.precio * el.unidades, 0)
 console.log(`el total a pagar es: $ {total}`)
